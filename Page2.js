7cdb34a97870f5f946c6fbab0113b3fceaf756f0
Snekha1202 <144783988+Snekha1202@users.noreply.github.com>
2024-05-16 18:52:23 +0530

    Page2.js

--- Page2.js
+++ Page2.js
@@ -4,20 +4,14 @@ import './App.css';
 const ReactTask2 = () =>{
 const Timer = () => {
   const [time, setTime] = useState(0);
-
-  // useEffect hook for initialization (componentDidMount)
   useEffect(() => {
     const intervalId = setInterval(() => {
       setTime((prevTime) => prevTime + 1);
     }, 1000);
-
-    // Cleanup function (componentWillUnmount)
     return () => {
       clearInterval(intervalId);
     };
-  }, []); // Empty dependency array ensures this effect runs only once, like componentDidMount
-
-  // useEffect hook for updating (componentDidUpdate)
+  }, []); 
   useEffect(() => {
     console.log('Time updated:', time);
   }, [time]); // Only runs when `time` changes
